d899fb84fb5684900edc321b09dac577593f633d
Salvador Subarroca <salvador@kiwity.com>
2015-02-27 12:01:12 +0100

    added grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,33 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: appConfig,
 
+    // Automatically update version
+    bump: {
+      // grunt bump:patch
+      // grunt bump:minor
+      // grunt bump:major
+      options: {
+        files: [
+          'package.json',
+          'bower.json'
+        ],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: [
+          'package.json',
+          'bower.json'
+        ],
+        createTag: true,
+        // tagName: 'v%VERSION%',
+        // tagMessage: 'Version %VERSION%',
+        push: false,
+        // pushTo: 'upstream',
+        // gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    },
+
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
